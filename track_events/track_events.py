53cfcf11f37f5960375ed0044f6320cc816a9cdd
Muralidharan <muraliselva.10@gmail.com>
2016-09-27 16:41:24 +0530

    Revamped Version with Twilio
    Revamped Version with Twilio

--- track_events/track_events.py
+++ track_events/track_events.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+#
+# Notification Generator for Openstack Events
+# 
+# Author: Muralidharan.S, Sundhar.S
+#
 import argparse
 import sys
 import ConfigParser
@@ -9,13 +15,15 @@ from kombu import BrokerConnection
 from kombu import Exchange
 from kombu import Queue
 from kombu.mixins import ConsumerMixin
+from twilio.rest import TwilioRestClient
+from keystoneclient.v2_0 import client as kclient
+from keystoneclient.auth.identity import v3
+from keystoneclient import session
 
 # Parsing input files
 parser = argparse.ArgumentParser()
 parser.add_argument("-e", "--event_conf", default=None,
                     help="Specify local track_event.conf file", metavar="FILE")
-parser.add_argument("-o", "--openstack_conf", default=None,
-                    help="Specify openstack conf file location(nova/neutron)", metavar="FILE")
 args, remaining_argv = parser.parse_known_args()
 
 # Processing Local conf file
@@ -24,15 +32,7 @@ if args.event_conf:
     config.read([args.event_conf])
     configdetails_section1 = dict(config.items("configdetails_section1"))
 
-# Processing openstack Conf file
-if args.openstack_conf:
-    config = ConfigParser.RawConfigParser()
-    config.read([args.openstack_conf])
-    openstackpath = config.read([args.openstack_conf])
-    oslo_messaging_rabbit = dict(config.items("oslo_messaging_rabbit"))
-
 # Necessary details which is needed to establish 
-# connection in common despite of arguments
 parser.set_defaults(**configdetails_section1)
 args = parser.parse_args(remaining_argv)
 LOG_FILE = args.log_file 
@@ -44,6 +44,33 @@ logging.basicConfig(filename=LOG_FILE, level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%m-%d %H:%M:%S')
 
+# Openstack Credentials for establishing Keystone connection
+tenant_name = args.tenant_name
+auth_url = args.auth_url
+username = args.username
+password = args.password
+region_name = args.region_name
+
+# Twilio Credentials
+account_sid = args.account_sid
+auth_token = args.auth_token
+from_mob_no = args.from_mob_no
+
+# Credentials for establishing Keystone Connection.
+kwargs_conn = {
+    "tenant_name":tenant_name,
+    "auth_url":auth_url,
+    "username":username,
+    "password":password,
+    "region_name": region_name
+}
+
+# Establish Connection With Keystone
+keystone = kclient.Client(**kwargs_conn)
+
+# Establish Twilio Connection
+tclient = TwilioRestClient(account_sid, auth_token)
+
 # calculating total number of arguments 
 # to decide config details fetching
 total_args = len(sys.argv)
@@ -54,21 +81,6 @@ if total_args == 2:
     parser.set_defaults(**configdetails_section1)
     args = parser.parse_args(remaining_argv)
     BROKER_URI = args.broker_uri
-
-# if there is three arguments were supplied 
-# (Local configuration file and neutron/nova conf supplied)
-elif total_args == 3:
-    parser.set_defaults(**oslo_messaging_rabbit)
-    args = parser.parse_args(remaining_argv)
-    
-    # Fetch rabbitmq uesrname and password
-    # from Nova/neutron file
-    username = args.rabbit_userid
-    password = args.rabbit_password
-    
-    # assign rabitmq username and passoword 
-    # to BROKER_URI
-    BROKER_URI = "amqp://" + username + ":" + password + "@localhost:5672/%2F"
 
 # class for tracking Events
 class TrackEvents(ConsumerMixin):
@@ -90,7 +102,6 @@ class TrackEvents(ConsumerMixin):
         queues = [queue1, queue2]
         return [ consumer(queues, callbacks=[ self.on_message ]) ]
 
-
     # actions to be performed on message 
     # receive on fanout nova
     def on_message(self, body, message):
@@ -110,100 +121,107 @@ class TrackEvents(ConsumerMixin):
 
         # creating oslo_message_dict 
         oslo_message_dict = json.loads(oslo_message)
-  
+ 
         # fetching event type
         event_type = oslo_message_dict['event_type']
+        payload = oslo_message_dict['payload']
+        tenant_id = payload['tenant_id']
+        dicts = {'event_type': event_type, 'tenant_id':tenant_id}
         try:
-            self.event_process(event_type)
+            self.event_process(dicts)
         except Exception, e:
             logging.info(repr(e))
             
     # Various event processes and actions
-    def event_process(self, event_type):
-        if event_type == "compute.instance.create.start":
-            self.instance_created_start_action()
+    def event_process(self, dicts):
+
+        tenant_id = dicts['tenant_id']
+        tenant_details = keystone.tenants.get(tenant_id)
+        mobile_number = tenant_details.mobile_number
+
+        if dicts['event_type'] == "compute.instance.create.start":
+            self.instance_created_start_action(mobile_number)
+
+        if dicts['event_type'] == "compute.instance.create.error":
+            self.instance_created_error_action(mobile_number)
             
-        if event_type == "compute.instance.create.error":
-            self.instance_created_error_action()
-            
-        if event_type == "compute.instance.create.end":
-            self.instance_created_end_action()
+        if dicts['event_type'] == "compute.instance.create.end":
+            self.instance_created_end_action(mobile_number)
            
-        if event_type == "compute.instance.delete.start":  
-            self.instance_deleted_start_action()
+        if dicts['event_type'] == "compute.instance.delete.start":  
+            self.instance_deleted_start_action(mobile_number)
    
-        if event_type == "compute.instance.delete.end":  
-            self.instance_deleted_end_action()
+        if dicts['event_type'] == "compute.instance.delete.end":  
+            self.instance_deleted_end_action(mobile_number)
    
-        if event_type == "compute.instance.resize.confirm.start":  
-            self.instance_resize_start_action()
-            
-        if event_type == "compute.instance.resize.confirm.end":  
-            self.instance_resize_end_action()
-            
-        if event_type == "volume.create.start":  
-            self.volume_create_start_action()            
+        if dicts['event_type'] == "volume.create.start":  
+            self.volume_create_start_action(mobile_number)            
 
-        if event_type == "volume.create.end":  
-            self.volume_create_end_action() 
+        if dicts['event_type'] == "volume.create.end":  
+            self.volume_create_end_action(mobile_number) 
             
-        if event_type == "volume.delete.start":  
-            self.volume_delete_start_action()            
+        if dicts['event_type'] == "volume.delete.start":  
+            self.volume_delete_start_action(mobile_number)            
 
-        if event_type == "volume.delete.end":  
-            self.volume_delete_end_action()
+        if dicts['event_type'] == "volume.delete.end":  
+            self.volume_delete_end_action(mobile_number)
     
-        if event_type == "volume.usage":  
-            self.volume_usage()            
-                   
     # actions to be performed on instance creation start
-    def instance_created_start_action(self):
-        logging.info("There is a new instance creation process started in devstack")
-        
-    # actions to be performed on instance creation error
-    def instance_created_error_action(self):
-        logging.info("There was a attempt to create new instance, But there was a error")
+    def instance_created_start_action(self, mobile_number):
+        message = tclient.messages.create(body="Instance Creation Process initiated Succesfully",
+                to=mobile_number,   
+                from_=from_mob_no)
+        logging.info("From number %s" % (from_mob_no)) 
+        logging.info("There is a new instance creation process initiated - Message SID: %s" % (message.sid))
         
     # actions to be performed on instance creation completion
-    def instance_created_end_action(self):
-        logging.info("New instance created succesfully in devstack")
+    def instance_created_end_action(self, mobile_number):
+        message = tclient.messages.create(body="Instance Creation Process Completed Succesfully",
+                to=mobile_number,    
+                from_=from_mob_no)
+        logging.info("There is a new instance creation process Completed Succesfully - Message SID: %s" % (message.sid))
 
     # actions to be performed on instance deletion start
-    def instance_deleted_start_action(self):
-        logging.info("Instance deletion action started in devstack")
-    
-    # actions to be performed on instance deletion
-    def instance_deleted_end_action(self):
-        logging.info("Instance deletion ends in devstack")
-        
-    # actions to be performed on instance resize start
-    def instance_resize_start_action(self):
-        logging.info("Instance resize action started in devstack")
-        
-    # actions to be performed on instance resize end
-    def instance_resize_end_action(self):
-        logging.info("Instance resize action completed in devstack")
-       
-    # actions to be performed on volume create start
-    def volume_create_start_action(self):
-        logging.info("Volume creation started in devstack")
-        
-    # actions to be performed on volume create end
-    def volume_create_end_action(self):
-        logging.info("Volume creation ends in devstack")
-    
-    # actions to be performed on volume deletion start
-    def volume_delete_start_action(self):
-        logging.info("Volume deletion started in devstack")
-        
-    # actions to be performed on volume deletion end
-    def volume_delete_end_action(self):
-        logging.info("Volume deletion ends in devstack")
+    def instance_deleted_start_action(self, mobile_number):
+        message = tclient.messages.create(body="Instance Deletion Process initiated Succesfully",
+                to=mobile_number,    
+                from_=from_mob_no)
+        logging.info("There is a instance deletion process initiated - Message SID: %s" % (message.sid))
 
-    # actions to be performed on volume usage details event
-    def volume_usage(self):
-        logging.info("Volume usage details")    
-  
+    # actions to be performed on instance deletion
+    def instance_deleted_end_action(self, mobile_number):
+        message = tclient.messages.create(body="Instance Deletion Process completed Succesfully",
+                to=mobile_number,    
+                from_=from_mob_no)
+        logging.info("There is a instance deletion process initiated - Message SID: %s" % (message.sid))
+
+    # actions to be performed on volume create start
+    def volume_create_start_action(self, mobile_number):
+        message = tclient.messages.create(body="Volume Creation Process initiated Succesfully",
+                to=mobile_number,    
+                from_=from_mob_no)
+        logging.info("There is a new Volume creation process initiated - Message SID: %s" % (message.sid))
+
+    # actions to be performed on volume create end
+    def volume_create_end_action(self, mobile_number):
+        message = tclient.messages.create(body="Volume Creation Process completed Succesfully",
+                to=mobile_number,    
+                from_=from_mob_no)
+        logging.info("There is a new Volume Creation process Completed - Message SID: %s" % (message.sid))
+
+    # actions to be performed on volume deletion start
+    def volume_delete_start_action(self, mobile_number):
+        message = tclient.messages.create(body="Volume Deletion Process initiated Succesfully",
+                to=mobile_number,    
+                from_=from_mob_no)
+        logging.info("There is a Volume deletion process initiated - Message SID: %s" % (message.sid))
+
+    # actions to be performed on volume deletion end
+    def volume_delete_end_action(self, mobile_number):
+        message = tclient.messages.create(body="Volume deletion Process Completed Succesfully",
+                to=mobile_number,
+                from_=from_mob_no)
+        logging.info("There is a Volume Deletion process Completed - Message SID: %s" % (message.sid))
 
 if __name__ == "__main__":
     logging.info("Connecting to broker {}".format(BROKER_URI))
